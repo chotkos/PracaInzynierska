83ca998af841ec698938245c799b90e80972de10
Mateusz Chodkowski <chotkos@gmail.com>
2015-11-22 21:50:44 +0100

    WYSIWYG  editor added

--- BlogSystem/client/postCreate/postcreateCtrl.js
+++ BlogSystem/client/postCreate/postcreateCtrl.js
@@ -1,3 +1,5 @@
 angular.module('blogSystem').controller('postcreateCtrl', ['$scope', '$meteor', '$state', function ($scope, $meteor, $state) {
     $scope.post = {};
+    $('#summernote').summernote();
+    //rozwiązanie     $('#summernote').code();
 }]);
