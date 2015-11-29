c71a8f53a10e880289aecbab680a4b81b19e8471
Mateusz Chodkowski <chotkos@gmail.com>
2015-11-30 00:02:56 +0100

    paginacja /posts

--- BlogSystem/client/postViews/list/postListCtrl.js
+++ BlogSystem/client/postViews/list/postListCtrl.js
@@ -1,9 +1,12 @@
 angular.module('blogSystem').controller('postListCtrl', ['$scope', '$meteor', '$state', 'postService', function ($scope, $meteor, $state, postService) {
 
+    $scope.page = 1;
     var sorters = {
         'sort': {
             'date': 1
-        }
+        },
+        'limit': 9,
+        'skip': 0,
     };
 
     var criterias = {};
@@ -14,6 +17,19 @@ angular.module('blogSystem').controller('postListCtrl', ['$scope', '$meteor', '$
         $state.go('post', {
             postId: _id
         });
+    };
+
+    $scope.nextPage = function () {
+        $scope.page++;
+        sorters.skip = 9 * ($scope.page - 1);
+        $scope.posts = postService.posts(criterias, sorters);
+
+    };
+
+    $scope.lastPage = function () {
+        $scope.page--;
+        sorters.skip = 9 * ($scope.page - 1);
+        $scope.posts = postService.posts(criterias, sorters);
     };
 
   }]);
